479e4b60a9adff8f32a21a91c78917acf0b54860
DMCDanteh <robert_buntic@hotmail.com>
2025-10-28 19:06:36 +0100

    E: Minor Fix to compute dt

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
     dt = 0
     
     while True:
+        dt = clock.tick(60) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -20,7 +21,7 @@ def main():
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
+
     
 
 if __name__ == "__main__":

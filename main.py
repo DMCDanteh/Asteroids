6a36b6e4dc3ccdefde5a5ef932f92fc33dfc0fa0
DMCDanteh <robert_buntic@hotmail.com>
2025-10-28 19:23:51 +0100

    H: Using groups to draw

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
     
     while True:
@@ -17,9 +24,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)        
+        updatable.update(dt)        
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        for a in drawable:
+            a.draw(screen)
+        
         pygame.display.flip()
 
     

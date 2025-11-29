cccfb9f90ebaa86bc38da0427c6a1e7bc304ec23
DMCDanteh <robert_buntic@hotmail.com>
2025-11-29 11:52:36 +0100

    Re added starting messages

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from shot import Shot
 
 def main():
     pygame.init()
+    print("Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
